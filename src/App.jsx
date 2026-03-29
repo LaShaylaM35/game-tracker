ac6ab4db0672f48232fc61515a9ae49efab07d27
LaShaylaM35 <lashaylam@yahoo.com>
2026-03-29 13:42:43 -0500

    changed the hash router back to browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import "./App.css";
 import { AddGamePage } from "./pages/AddGamePage";
 import { NavBar } from "./components/NavBar";
 
-
-
-
-
 function App() {
   return (
     <>
-      <NavBar />
-      <HashRouter>
+      <BrowserRouter>
+        <NavBar />
+
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/addgamepage" element={<AddGamePage />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
